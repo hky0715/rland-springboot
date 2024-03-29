47d26af361825658c68e95f79191db0bcda12dd1
hky0715 <hky0715@gmail.com>
2024-03-29 14:39:44 +0900

    register menu by admin

--- src/main/java/kr/co/rland/web/controller/admin/MenuController.java
+++ src/main/java/kr/co/rland/web/controller/admin/MenuController.java
@@ -2,7 +2,12 @@ package kr.co.rland.web.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import kr.co.rland.web.entity.Menu;
 
 @Controller("adminMenuController")
 @RequestMapping("admin/menu")
@@ -11,6 +16,30 @@ public class MenuController {
     @GetMapping("reg")
     public String reg() {
         return "admin/menu/reg";
+    }
+
+    @PostMapping("reg")
+    public String reg(
+        // String korName
+        //                 , String engName
+        //                 , MultipartFile imgFile
+        //                 , Integer price
+                        Menu menu
+                        // binary 전송방식! 맨땅!
+                        , @RequestParam("img-file") MultipartFile imgFile) {
+        
+        String fileName = imgFile.getOriginalFilename();
+        System.out.println(fileName);
+        // Menu menu = Menu.builder()
+        //             .korName(korName)
+        //             .engName(engName)
+        //             .price(price)
+        //             .img(imgFile.getOriginalFilename())
+        //             .build();
+
+        System.out.println(menu);
+
+        return "redirect:list";
     }
 
     @GetMapping("list")
