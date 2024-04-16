9e84877f65917848507c0020eae5d96a93160956
Hong Kyuyoung <hky0715@gmail.com>
2024-04-16 14:57:57 +0900

    Implementing JDBCUserDetailServices

--- src/main/java/kr/co/rland/web/config/WebSecurityConfig.java
+++ src/main/java/kr/co/rland/web/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -34,8 +35,25 @@ public class WebSecurityConfig {
 		return http.build();
 	}
 
-    @Bean
-    // 사용자 정보를 주는 서비스
+	@Bean
+	// 데이터베이스 쿼리를 해서 사용자 정보를 제공하는 제공자
+	public UserDetailsService jdbcUserDetailsService() {
+//      -> 결과 집합의 모양
+//         ┌────────────┬───────────┬─────────┐
+//         │  username  │  password │ enabled │
+//         ├────────────┼───────────┼─────────┤
+//         │   newlec   │    111    │    1    │
+		String userSql = "select username, pwd password, 1 enabled from member where username = ''";
+		String authorSql = "";
+
+		//너는 쿼리만 줘라
+		JdbcUserDetailsManager manager = new JdbcUserDetailsManager(userSql);
+
+		return manager;
+	}
+
+    // @Bean
+    // 메모리상에서 사용자 정보를 주는 서비스. 이 bean을 지워버리면 jdbc로 갖고와야징
     public UserDetailsService userDetailsService() {
         // 사용자 정보를 담은 객체
         UserDetails user = User.builder()
