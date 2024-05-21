0d8e4f76ab45d655214edb5b04e08d4bf113b9e4
hky0715 <hky0715@gmail.com>
2024-05-21 11:54:55 +0900

    Feat: Web Socket Chatting

--- src/main/resources/static/js/chat/room.js
+++ src/main/resources/static/js/chat/room.js
@@ -1,21 +1,31 @@
-window.addEventListener("load", function() {
-    const chatWindow = this.document.querySelector("#chat-window");
-    const connButton = chatWindow.querySelector(".btn-conn");
-    const textInput = chatWindow.querySelector("input[type=text]");
-    const ul = chatWindow.querySelector("ul");
+const chatWindow = document.querySelector("#chat-window");
+const connButton = chatWindow.querySelector(".btn-conn");
+const textInput = chatWindow.querySelector("input[name='message']");
+const ul = chatWindow.querySelector("ul");
+const sendBtn = chatWindow.querySelector(".send");
 
-    connButton.onclick = function() {
-        const socket = new WebSocket("ws://localhost:8080/chat");
-        socket.onopen = ()=>{
-            let li = `<li>서버에 접속되었습니다!</li>`;
-            ul.insertAdjacentHTML("beforeend", li);
-            textInput.disabled = false;
-        };
-        socket.onclose = ()=>{};
-        socket.onmessage = (e)=>{
-            let li = `<li>${e.data}</li>`
-            ul.insertAdjacentHTML("beforeend", li);
-        };
+let sock = null;
 
-    }
-})
+connButton.onclick=function(){
+    // sock = new WebSocket("ws://192.168.0.54:8080/chat");
+    sock = new WebSocket("ws://192.168.0.75:8082/chat");
+    // sock = new WebSocket("ws://localhost:8082/chat");
+    sock.onopen = (e) => {
+        let li = `<li>서버에 연결되었습니다.</li>`;
+        ul.insertAdjacentHTML("beforeend", li);
+        textInput.disabled=false;
+    };
+    sock.onclose = () => {};
+    sock.onmessage = (e) => {
+        let li = `<li>${e.data}</li>`;
+        ul.insertAdjacentHTML("beforeend", li);
+    };
+}
+
+sendBtn.onclick=function(){
+    if(!sock)
+        return;
+
+    let text = textInput.value;
+    sock.send(text);
+}
